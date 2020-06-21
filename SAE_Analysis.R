374073040c5262a6b2bcb9ba7d54e4228b5f41c9
twang-stats <66153092+twang-stats@users.noreply.github.com>
2020-06-21 15:36:20 -0400

    Unnesting of lists to clean data

--- SAE_Analysis.R
+++ SAE_Analysis.R
@@ -1,5 +1,5 @@
 library(jsonlite)
-library(dplyr)
+library(tidyverse)
 library(rms)
 library(brms)
 library(bayesplot)
@@ -12,36 +12,22 @@ download.file(link, temp)
 
 #unzip file and flatten relevant JSON as much as possible initially
 json_data_source <- fromJSON(unz(temp, 'animalandveterinary-event-0001-of-0001.json'), flatten=TRUE)
-json_data <- flatten(json_data_source$results)
+json_data <- json_data_source$results
 
 
 #Extract any observation classified as a serious adverse event
-#Remove null rows of things like drug expiry (we want health events only here)
-drug_data <- (as.data.frame(json_data[json_data$serious_ae=='true', ])
-              %>% filter(reaction != "NULL"))
+#Provided that a reaction was recorded and at least 1 animal was affected
+json_data_stage1 <- (as.data.frame(json_data[json_data$serious_ae=='true', ])
+              %>% filter(reaction != "NULL")
+              %>% filter(number_of_animals_affected != "NA"))
 
-#Reaction column contains multiple events and descriptors per animal
+#Reaction and drug columns contain multiple events and descriptors per reported instance
 #map each event out individually as one distinct row of data
-#Loop over the rest of the dataset to do so
-#Note that the drug column is like this as well
 
-clean2 <- c()
-for (i in 1:(dim(drug_data)[1])){
-  sae <- drug_data$reaction[[i]]
-  ainfo <- drug_data[i, -c(1)] %>% slice(rep(1, dim(sae)[1]))
-  clean2 <- bind_rows(clean2, bind_cols(sae, ainfo))
-}
+json_data_stage2 <- (json_data_stage1 %>% unnest_wider(outcome, simplify = TRUE, names_repair = "universal") 
+                     %>% unnest_wider(drug, simplify = TRUE)
+                     %>% unnest_longer(reaction, simplify=TRUE))
 
-#Extract drug information
-#Row 11 is problematic - entered as list, all other rows are as characters
-
-rows <- c()
-for (i in 1:11){
-  newdrug <- unlist(drug[i], recursive = FALSE)
-  rows <- bind_rows(rows,drug[i])
-}
-
-  
 #Objective: find serious adverse events with known outcomes, see if relationship exists between outcome and drug/breed, age, weight, etc.
 #Multinomial logistic regression, clustering/classification, neural networks possible, though sparsity is an issue
 #Consider regularization/shrinkage in approaches
